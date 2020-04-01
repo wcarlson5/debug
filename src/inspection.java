4bad8cd4dbba81ad39a2d826330921469d32b1e9
Walker Carlson <wcarlson@gmail.com>
2020-04-01 13:04:46 -0700

    updated debug

---
+++ src/inspection.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class inspection {
+
+    /* Without changing the code what is the contents of a at the end of a run?
+    */
+    public static void main(String[] args){
+        int[] a = new int[10];
+        Random rand = new Random(10);
+        for(int i = 0; i < a.length; i++){
+            a[i] = rand.nextInt(10);
+        }
+        System.out.print(a);
+    }
+}
--- src/inspection2.java
+++ src/inspection2.java
@@ -1,15 +1,19 @@
 import java.util.Random;
 
 public class inspection2 {
-
-    /* Without changing the code what is the contents of a at the end of a run?
-    */
+    /* Without changing the code what is the length of a, and what is the value of i?
+     */
     public static void main(String[] args){
-        int[] a = new int[10];
-        Random rand = new Random(10);
-        for(int i = 0; i < a.length; i++){
-            a[i] = rand.nextInt(10);
-        }
+
+        Random rand = new Random(11);
+        int[] a = new int[len(rand)];
         System.out.print(a);
+
+    }
+
+    private static int len(Random rand){
+        int i = rand.nextInt(10000) + 5;
+        i++;
+        return rand.nextInt(i);
     }
 }
